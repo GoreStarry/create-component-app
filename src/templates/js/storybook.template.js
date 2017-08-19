1dda0551f9cace58b22547ff3db4fe83b83635ff
Gore Wang <sunrise91.t3@gmail.com>
2017-08-19 17:37:31 +0800

    adjust .stroies import path

--- src/templates/js/storybook.template.js
+++ src/templates/js/storybook.template.js
@@ -2,7 +2,7 @@ function generateStorybookTemplate(COMPONENT_NAME) {
   return `import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import ${COMPONENT_NAME} from './${COMPONENT_NAME}'
+import ${COMPONENT_NAME} from '../index.js'
 
 storiesOf('${COMPONENT_NAME}', module).add('Example 1', () =>
   <${COMPONENT_NAME} />
